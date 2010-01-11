bdea1ebcd90176824206cf75bc14a10f84f01cc6
Hints of Grape <hintsofgrape@gmail.com>
2010-01-10 16:17:13 -0800

    Pretty-fy the site with custom.css, add basic routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title
+    base_title = "hints of grape"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+    
+  def logo
+    logo = image_tag("logo1.png", :alt => "logo", :class => "round")
+  end
 end
